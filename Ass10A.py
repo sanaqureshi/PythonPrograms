2eaa52686d77fb9d398e40e0004c7c4ce75f9fad
root <root@python.c.hardy-symbol-169914.internal>
2017-06-11 13:43:07 +0000

    test

--- Ass10A.py
+++ Ass10A.py
@@ -1,11 +1,1 @@
-def countStringLength(inputString):
- """Function to Compute the Length of a Given String : """
-count = 0
-x = 'Qureshi'
-for i in x:
-	count = count+1
-	print 'The Length of the given String is :' ,str(count)
-
-print "Function to Compute the Length of The Input String : "
-inputString = input()
-countStringLength(inputString)
+Hii
