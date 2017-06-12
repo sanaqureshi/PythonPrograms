7ac6850663324245e989e4d76ce675ef1d1a7586
root <root@python.c.hardy-symbol-169914.internal>
2017-06-12 13:47:02 +0000

    initial context

--- Ass1.py
+++ Ass1.py
@@ -1,6 +1,8 @@
+# 1. Write a python script for creating directory,displaying its contents. 
+
 import os,sys
-os.mkdir("Sana")
-os.chdir("Sana")
-os.system("touch file1")
+os.mkdir("SanaDir")
+os.chdir("SanaDir")
+os.system("touch file5")
 os.system("ls")
-os.system("cat file1")
+os.system("cat file5")
